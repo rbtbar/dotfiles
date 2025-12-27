bf949117b24cd9a909b986677d71a120f8b382e6
Robert <robert@rbsoftware.pl>
2025-12-27 12:03:17 +0100

    Lua lsp

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -271,6 +271,17 @@ require("lazy").setup({
 
         vim.lsp.config("lua_ls", {
           capabilities = capabilities,
+          settings = {
+            Lua = {
+              runtime = { version = "LuaJIT" },
+              diagnostics = { globals = { "vim" } },
+              workspace = {
+                library = vim.api.nvim_get_runtime_file("", true),
+                checkThirdParty = false,
+              },
+              telemetry = { enable = false },
+            },
+          },
         })
 
         vim.lsp.config("ts_ls", {
